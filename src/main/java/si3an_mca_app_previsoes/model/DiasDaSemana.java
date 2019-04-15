0b3047c659901130c31baefa99764539b692cd53
LeonardoAraujo97 <leonardoaraujo97@outlook.com>
2019-04-14 21:38:40 -0300

    [FIX] Relacionamento corrigido.

--- src/main/java/si3an_mca_app_previsoes/model/DiasDaSemana.java
+++ src/main/java/si3an_mca_app_previsoes/model/DiasDaSemana.java
@@ -1,10 +1,14 @@
 package si3an_mca_app_previsoes.model;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class DiasDaSemana {
 
+	@Id
+	@NotNull
 	private String diaDaSemana;
 	
 
